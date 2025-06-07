081adcac484c2b9d8cda22914da5eee89bc84cc7
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-06-07 09:28:04 +0200

    fix(neovim): reduce treesitter complexity

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter", -- context aware syntax tree
+    "nvim-treesitter/nvim-treesitter",                 -- context aware syntax tree
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects", -- additional text objects
         "JoosepAlviste/nvim-ts-context-commentstring", -- treesitter aware comment string
-        "lewis6991/spellsitter.nvim", -- treesitter enabled spell check where it makes sense
-        "andymass/vim-matchup", -- better matching of keywords and blocks
+        "lewis6991/spellsitter.nvim",                  -- treesitter enabled spell check where it makes sense
+        "andymass/vim-matchup",                        -- better matching of keywords and blocks
     },
     build = ":TSUpdate",
     event = "BufReadPost",
     opts = {
         ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-        ignore_install = { "" }, -- List of parsers to ignore installing
+        sync_install = false,     -- install languages synchronously (only applied to `ensure_installed`)
+        ignore_install = { "" },  -- List of parsers to ignore installing
         autopairs = {
             enable = true,
         },
@@ -40,44 +39,5 @@ return {
                 node_decremental = "grm",
             },
         },
-        textobjects = {
-            -- TODO` do I need this? really need to figure out better mappings`
-            select = {
-                enable = true,
-                -- Automatically jump forward to textobj, similar to targets.vim
-                lookahead = true,
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
-                },
-            },
-        },
     },
-    init = function()
-        vim.opt.foldmethod = "expr"
-        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    end
 }
