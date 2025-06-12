82257d9ec4adde8e7102619f3755f9c79dcc6851
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-06-12 21:46:29 +0200

    remove session autocommands

--- dot_config/nvim/lua/plugins/sessions.lua
+++ dot_config/nvim/lua/plugins/sessions.lua
@@ -1,4 +1,6 @@
 return {
+    -- TODO: better, more automated session handling
+    --       and also better setup code
     "olimorris/persisted.nvim",
     event = "VeryLazy",
     config = function()
@@ -9,32 +11,6 @@ return {
             use_git_branch = true,
             branch_separator = "_",
         })
-        local group = vim.api.nvim_create_augroup("PersistedHooks", {})
-
-        vim.api.nvim_create_autocmd({ "User" }, {
-            pattern = "PersistedLoadPre",
-            group = group,
-            callback = function()
-                vim.lsp.stop_client(vim.lsp.get_clients())
-                vim.cmd("edit")
-            end,
-        })
-        vim.api.nvim_create_autocmd({ "User" }, {
-            pattern = "PersistedTelescopeLoadPre",
-            group = group,
-            callback = function()
-                vim.cmd("bufdo bwipeout")
-            end,
-        })
-        vim.api.nvim_create_autocmd({ "User" }, {
-            pattern = "PersistedTelescopeLoadPost",
-            group = group,
-            callback = function(session)
-                pcall(vim.cmd, "git checkout " .. session.branch)
-            end,
-        })
-
-
         require("telescope").load_extension("persisted")
         vim.keymap.set("n", "<leader>ss", "<cmd>Telescope persisted<cr>", { desc = "Search Sessions" })
         vim.keymap.set("n", "<leader>tS", "<cmd>SessionToggle<cr>", { desc = "Toggle Sessions" })
