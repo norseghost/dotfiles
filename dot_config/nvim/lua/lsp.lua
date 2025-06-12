eade122f00d3764d6db3d7cf07023065312c24e1
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-06-12 22:28:33 +0200

    Revert "lsp: try new way of autoformat etc"
    This reverts commit b285641201d938e402df5f41f19bd94dcfa10208.

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,8 +1,8 @@
--- enable
+-- enable  
 local function lsp_highlight_document(client, bufnr)
     -- Set autocommands conditional on server_capabilities
     if client.server_capabilities.documentHighlightProvider then
-        --[[ if client.supports_method("textDocument/document Highlight") then ]]
+        --[[ if client.supports_method("textDocument/documentHighlight") then ]]
         vim.api.nvim_create_augroup("lsp_document_highlight", {
             clear = false
         })
@@ -36,50 +36,20 @@ local lsp_autoformat = function(client, bufnr)
     end
 end
 
--- vim.api.nvim_create_autocmd("LspAttach", {
---     group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
---     callback = function(ev)
---         local client = vim.lsp.get_client_by_id(ev.data.client_id)
---         lsp_autoformat(client, ev.buf)
---         lsp_highlight_document(client, ev.buf)
---         -- enable lsp completion
---         vim.lsp.completion.enable(
---             true, client.id, ev.buf, { autotrigger = true }
---         )
---     end,
--- })
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("my.lsp", {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method("textDocument/implementation") then
-            -- Create a keymap for vim.lsp.buf.implementation ...
-        end
-
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-        if client:supports_method("textDocument/completion") then
-            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            -- client.server_capabilities.completionProvider.triggerCharacters = chars
-
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        end
-
-        -- Auto-format ("lint") on save.
-        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if not client:supports_method("textDocument/willSaveWaitUntil")
-            and client:supports_method("textDocument/formatting") then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-                buffer = args.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
+    group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        lsp_autoformat(client, ev.buf)
+        lsp_highlight_document(client, ev.buf)
+        -- enable lsp completion
+        vim.lsp.completion.enable(
+            true, client.id, ev.buf, { autotrigger = true }
+        )
     end,
 })
 
+
 -- enable configured language servers
 -- you can find server configurations from lsp/*.lua files
-vim.lsp.enable("lua_ls")
+vim.lsp.enable('lua_ls')
