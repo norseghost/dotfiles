8db07f68c1973440d24a4870e3a7629434a2b23b
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-06-13 09:56:58 +0200

    set gitsigns priority to 0

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -26,7 +26,7 @@ return {
                 delay = 1000,
                 ignore_whitespace = false,
             },
-            sign_priority = 6,
+            sign_priority = 0,
             update_debounce = 100,
             status_formatter = nil, -- Use default
             max_file_length = 40000,
