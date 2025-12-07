c20240e7830280860c02c04db4c2a64ebc9c00a0
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-07 12:04:17 +0100

    add zsh

---
+++ dot_config/zsh/zshrc.d/20-fzf.zsh
@@ -0,0 +1,5 @@
+# FZF - fuzzy search
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
+
+export FZF_DEFAULT_COMMAND='rg --files --hidden --smart-case --follow '
---
+++ dot_config/zsh/zshrc.d/21-util_alias.zsh
@@ -0,0 +1,11 @@
+alias du="ncdu --color dark -rr -x --exclude .git"
+alias ls="exa --group-directories-first"
+alias ll="ls -l"
+alias la='ls -a'
+alias lla='ls -al'
+alias tree='ls --tree'
+alias cat="bat --style='plain'"
+alias man='tldr'
+alias ping='prettyping'
+alias f='fzf --preview "bat --color \"always\" {}"'
+
