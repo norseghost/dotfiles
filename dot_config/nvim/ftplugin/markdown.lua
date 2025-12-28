0641086bd42bcb5e5c9630dcc65323dc158ac4d0
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-29 00:13:10 +0100

    markdown list continuation

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,9 @@
-vim.opt.formatoptions:remove { "t" }
-vim.opt.wrap = false
+-- n: recognize numbered lists
+-- 2: use the indent of the second line of a paragraph for the rest
+-- t: auto-wrap text using textwidth
+-- c: auto-wrap lists/comments
+vim.opt_local.formatoptions = "n2tcoqj"
+
+-- Tell Neovim what the markers are so it knows how much to indent
+vim.opt_local.comments = "b:-,b:*,b:+,b:1.,n:>"
+
