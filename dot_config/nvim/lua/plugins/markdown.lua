229035a6c407ea7dc7743891ed5273dcc44e2506
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-29 00:35:58 +0100

    different markdown list continuation

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -50,40 +50,17 @@ return {
         end
     },
     {
-    "gaoDean/autolist.nvim",
-    ft = {
-        "markdown",
-        "text",
-        "tex",
-        "plaintex",
-        "norg",
-        "pandoc",
-    },
-    config = function()
-        require("autolist").setup()
-
-        vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-        vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-        -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-        vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-        vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-        vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-        vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-        vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
-
-        -- cycle list types with dot-repeat
-        vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-        vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
-
-        -- if you don't want dot-repeat
-        -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
-        -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
-
-        -- functions to recalculate list on edit
-        vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-        vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-        vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-        vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
-    end,
-},
+        "tadmccorkle/markdown.nvim",
+        ft = "markdown", -- or 'event = "VeryLazy"'
+        opts = {
+            on_attach = function(bufnr)
+                local map = vim.keymap.set
+                local opts = { buffer = bufnr }
+                map({ 'n', 'i' }, '<C-CR>', '<Cmd>MDListItemBelow<CR>', opts)
+                map({ 'n', 'i' }, '<M-C-CR>', '<Cmd>MDListItemAbove<CR>', opts)
+                map('n', '<M-c>', '<Cmd>MDTaskToggle<CR>', opts)
+                map('x', '<M-c>', ':MDTaskToggle<CR>', opts)
+            end,
+        },
+    }
 }
