1c14c0ef7dbed2f4be12d36a475829da6fe5e1d5
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-30 16:13:59 +0100

    neovim: rework session saving

--- dot_config/nvim/lua/plugins/sessions.lua
+++ dot_config/nvim/lua/plugins/sessions.lua
@@ -1,16 +1,20 @@
-return {
+local sessions = {
     "olimorris/persisted.nvim",
     event = "VeryLazy",
-    config = function()
-        require("persisted").setup({
-            autoload = true,
-            -- Resolves to ~/.local/share/nvim/sessions/
-            save_dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
-            use_git_branch = true,
-            branch_separator = "_",
-        })
-        require("telescope").load_extension("persisted")
-        vim.keymap.set("n", "<leader>ss", "<cmd>Telescope persisted<cr>", { desc = "Search Sessions" })
-        vim.keymap.set("n", "<leader>tS", "<cmd>SessionToggle<cr>", { desc = "Toggle Sessions" })
-    end
+    opts = {
+        autoload = true,
+        on_autoload_no_session = function()
+            vim.notify("No existing session to load.")
+        end,
+        -- Resolves to ~/.local/share/nvim/sessions/
+        save_dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
+        use_git_branch = true,
+        branch_separator = "_",
+    },
+    keys = {
+        { "<leader>tS", "<cmd>SessionToggle<cr>",       desc = "Toggle Sessions" },
+        { "<leader>ss", "<cmd>Telescope persisted<cr>", desc = "Search Sessions" }
+    }
 }
+
+return sessions
