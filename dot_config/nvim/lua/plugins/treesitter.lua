7b94dd5300d55b6aa9e89109285d4610785eccdb
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-30 22:11:42 +0100

    markdown mapping fixes

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,42 @@
 return {
-    "nvim-treesitter/nvim-treesitter",                 -- context aware syntax tree
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring", -- treesitter aware comment string
-        "lewis6991/spellsitter.nvim",                  -- treesitter enabled spell check where it makes sense
-        "andymass/vim-matchup",                        -- better matching of keywords and blocks
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        "lewis6991/spellsitter.nvim",
+        "andymass/vim-matchup",
     },
     build = ":TSUpdate",
     event = "BufReadPost",
+    config = function(_, opts)
+        -- FIX 1: Explicitly use zig as the compiler
+        require("nvim-treesitter.install").compilers = { "zig" }
+        require("nvim-treesitter.configs").setup(opts)
+    end,
     opts = {
-        ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        sync_install = false,     -- install languages synchronously (only applied to `ensure_installed`)
-        ignore_install = { "" },  -- List of parsers to ignore installing
-        autopairs = {
+        -- FIX 2: Avoid "all" on Windows. List what you actually use.
+        -- If you must have all, keep it, but expect long install times.
+        compilers             = { "zig", "gcc", "clang" },
+        ensure_installed      = {
+            "lua", "vim", "vimdoc", "query", "javascript",
+            "typescript", "c", "cpp", "python", "markdown", "markdown_inline",
+            "ruby", "powershell", "bash"
+        },
+
+        -- FIX 3: Set to true. This installs parsers one-by-one.
+        -- This prevents the "Paging file too small" error by not spawning 50 git/zig jobs at once.
+        sync_install          = true,
+        ignore_install        = {},
+        autopairs             = { enable = true },
+        matchup               = { enable = true },
+        highlight             = {
             enable = true,
         },
-        matchup = {
-            enable = true, -- mandatory, false will disable the whole extension
-        },
-        highlight = {
-            enable = true, -- false will disable the whole extension
-            -- disable = { "beancount" }, -- list of language that will be disabled
-            -- additional_vim_regex_highlighting = { "markdown" },
-        },
-        indent = {
+        indent                = {
             enable = true,
-            -- disable = { "python", "yaml" }
         },
-        context_commentstring = {
-            enable = true,
-            enable_autocmd = false,
-        },
+        -- NOTE: context_commentstring setup has changed in newer versions
+        -- You usually don't need the 'enable = true' block here anymore
+        -- if using the newer JoosepAlviste plugin.
         incremental_selection = {
             enable = true,
             keymaps = {
