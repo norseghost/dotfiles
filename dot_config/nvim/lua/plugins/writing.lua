9cb0e30cf243516d0df8601ce9cbaf2a3a8742ee
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2025-12-30 16:52:32 +0100

    neovim: add static site gen helper

---
+++ dot_config/nvim/lua/plugins/writing.lua
@@ -0,0 +1,15 @@
+local writing = {
+    {
+        "jaytyrrell13/static.nvim",
+        opts = {
+            strategy = "neovim"
+        },
+        keys = {
+            { "<leader>Sb", ":Static build<cr>" },
+            { "<leader>Ss", ":Static serve<cr>" },
+            { "<leader>Sp", ":Static prod<cr>" },
+        },
+    }
+}
+
+return writing
