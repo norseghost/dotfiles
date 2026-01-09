f63607fb582305a1de64ba3d26d80736f7b989af
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2026-01-09 21:37:53 +0100

    lsp: turn off harper spell check

--- dot_config/nvim/lsp/harper_ls.lua
+++ dot_config/nvim/lsp/harper_ls.lua
@@ -4,16 +4,6 @@ return {
         harper = {
             linters = {
                 SpellCheck = false,
-                SpelledNumbers = false,
-                AnA = true,
-                SentenceCapitalization = true,
-                UnclosedQuotes = true,
-                WrongQuotes = false,
-                LongSentences = true,
-                RepeatedWords = true,
-                Spaces = true,
-                Matcher = true,
-                CorrectNumberSuffix = true
             },
             dialect = "american",
         },
