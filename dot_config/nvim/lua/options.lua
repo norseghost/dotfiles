997e460cc23085a3db65d00a985af1ca013a2408
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2026-04-14 16:19:15 +0200

    [neovim] cmp: tweak completion limits

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -149,11 +149,11 @@ o.hlsearch = true          -- highlight search results
 -- completion
 o.autocomplete = true
 o.complete = {
-    "o^9",
+    "o",
     ".^3",
     "w^3",
     "b^3",
-    "kspell^5"
+    "kspell^3"
 }
 o.completeopt = {
     "menu",     -- show completion menu
