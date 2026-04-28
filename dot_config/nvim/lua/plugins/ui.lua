14933fadd7e28b537cdb9f579313681297c00be3
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2026-04-28 08:34:36 +0200

    [neovim] try modus_operandi on e ink screen_

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,15 +1,25 @@
 return {
-    { -- color scheme
-        "gbprod/nord.nvim",
-        lazy = false,
-        priority = 1000,
-        init = function()
-            if not IS_TERMUX then
-                vim.cmd.colorscheme("nord")
-            else
-                vim.cmd.colorscheme("wordsmith")
-            end
+    function()
+        if not IS_TERMUX then
+            return {
+                "gbprod/nord.nvim",
+                lazy = false,
+                priority = 1000,
+                init = function()
+                    vim.cmd.colorscheme("nord")
+                end
+            }
+        else
+            return {
+                "miikanissi/modus-themes.nvim",
+                lazy = false,
+                priority = 1000,
+                init = function()
+                    vim.cmd.colorscheme("modus_operandi")
+                    -- vim.cmd.colorscheme("wordsmith")
+                end
+            }
         end
-    },
+    end,
     "kyazdani42/nvim-web-devicons", -- file type icon
 }
