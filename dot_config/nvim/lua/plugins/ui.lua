48e253e97063fe0027ce718fd8e47d35a86b43db
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2026-04-28 22:47:52 +0200

    [neovim] fix theme selector
    cannot use anonymous functions to return dicts apparently

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,25 +1,12 @@
 return {
-    function()
-        if not IS_TERMUX then
-            return {
-                "gbprod/nord.nvim",
-                lazy = false,
-                priority = 1000,
-                init = function()
-                    vim.cmd.colorscheme("nord")
-                end
-            }
-        else
-            return {
-                "miikanissi/modus-themes.nvim",
-                lazy = false,
-                priority = 1000,
-                init = function()
-                    vim.cmd.colorscheme("modus_operandi")
-                    -- vim.cmd.colorscheme("wordsmith")
-                end
-            }
-        end
-    end,
-    "kyazdani42/nvim-web-devicons", -- file type icon
+    {
+        IS_TERMUX and "miikanissi/modus-themes.nvim" or "gbprod/nord.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local theme = IS_TERMUX and "modus_operandi" or "nord"
+            vim.cmd.colorscheme(theme)
+        end,
+    },
+    "kyazdani42/nvim-web-devicons",
 }
